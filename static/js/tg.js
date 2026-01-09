73bda167d7f3b591ca321d1b69c09bab8f805f55
OFFpolice <offpolice2077@gmail.com>
2026-01-09 13:27:50 +0200

    Update tg.js

--- static/js/tg.js
+++ static/js/tg.js
@@ -4,7 +4,6 @@ window.addEventListener("load", () => {
     const tg = window.Telegram.WebApp;
 
     tg.ready();
-    //tg.expand();
     tg.enableClosingConfirmation();
     tg.disableVerticalSwipes();
     tg.requestFullscreen();
