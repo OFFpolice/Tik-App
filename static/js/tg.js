70c035f722ebd4d28b4684eec4a71a18c2ca5878
OFFpolice <offpolice2077@gmail.com>
2026-01-09 13:21:41 +0200

    Create static/js/tg.js

---
+++ static/js/tg.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", () => {
+
+  if (window.Telegram && window.Telegram.WebApp) {
+    const tg = window.Telegram.WebApp;
+
+    tg.ready();
+    //tg.expand();
+    tg.enableClosingConfirmation();
+    tg.disableVerticalSwipes();
+    tg.requestFullscreen();
+    tg.lockOrientation();
+
+    console.log("Telegram WebApp is ready:", tg.initData);
+  } else {
+    console.warn("Telegram WebApp not found.");
+  }
+});
